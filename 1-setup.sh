81a9d790a7e7e68d4388c1d13ea206d415ebebd1
Rushi <44952533+Skyliegirl33@users.noreply.github.com>
2021-11-21 02:09:47 +0100

    Fix pacman.conf

--- 1-setup.sh
+++ 1-setup.sh
@@ -253,9 +253,10 @@ fi
 # Add chaotic-aur
 pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key FBA220DFC880C036
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+pacman -Sy --noconfirm
 
-cat <<EOF > /etc/pacman.conf
+cat >> /etc/pacman.conf <<'EOF'
 [chaotic-aur]
 Include = /etc/pacman.d/chaotic-mirrorlist
 EOF
